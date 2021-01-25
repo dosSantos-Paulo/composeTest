4a31b466b44ff2f8122b240d20e8ac92e4451ad9
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2021-01-24 21:07:26 -0300

    add botoes a tela formulario

--- app/src/main/java/com/devdossantos/compose/FormActivity.kt
+++ app/src/main/java/com/devdossantos/compose/FormActivity.kt
@@ -2,10 +2,16 @@ package com.devdossantos.compose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.colorResource
@@ -23,8 +29,8 @@ class FormActivity : AppCompatActivity() {
 
     @Composable
     private fun FormContent() {
-        Scaffold(topBar = {FormTopBar()}) {
-            
+        Scaffold(topBar = { FormTopBar() }) {
+            FormFields()
         }
     }
 
@@ -40,6 +46,45 @@ class FormActivity : AppCompatActivity() {
             backgroundColor = colorResource(id = R.color.purple_500),
             contentColor = Color.White
         )
+    }
+
+    @Composable
+    private fun FormFields() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            OutlinedTextField(value = "",
+                onValueChange = {},
+                label = { Text(text = stringResource(id = R.string.name_hint)) })
+
+            OutlinedTextField(value = "",
+                onValueChange = {},
+                label = { Text(text = stringResource(id = R.string.email_hint)) })
+
+            DropdownMenu(
+                toggle = {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        TextButton(onClick = { /*TODO*/ }) {
+                            Text(text = stringResource(id = R.string.role_select))
+                        }
+
+                        Text(text = "Desconhecido")
+                    }
+                }, expanded = false, onDismissRequest = { /*TODO*/ }) {
+
+                DropdownMenuItem(onClick = { /*TODO*/ }) {
+
+                }
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.add_button_text))
+            }
+        }
     }
 
     @Preview
